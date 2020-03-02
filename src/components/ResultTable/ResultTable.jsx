3d70bd805a76f8661e0abdb3f3180460ca3541fe
OllenkA <shamko.olga2018@gmail.com>
2020-03-03 01:28:22 +0300

    change styles ResultTable

--- src/components/ResultTable/ResultTable.jsx
+++ src/components/ResultTable/ResultTable.jsx
@@ -3,46 +3,37 @@ import styles from './ResultTable.module.css';
 
 function ResultTable({games, gamer1, gamer2, allPointsGamer1, allPointsGamer2}) {
 
-    return <div className={styles.container}>
-        <section>
-            <h2>Result table</h2>
-        </section>
+    return <table className={styles.container} cellPadding={5}>
+
+        <caption className={styles.headlineTable}>Result table</caption>
         <thead>
         <tr>
-            <th className={styles.table}>Number round</th>
-            <th className={styles.table}>{gamer1}</th>
-            <th className={styles.table}>{gamer2}</th>
+            <th>Number round</th>
+            <th>{gamer1}</th>
+            <th>{gamer2}</th>
         </tr>
 
         {games.map(g => {
-                    return (
-                        <tr key={g.id}>
-                            <td className={styles.table}>
-                                {g.id}
-                            </td>
-                            <td className={styles.table}>
-                                {g.winnerPointsGamer1}
-                            </td>
-                            <td className={styles.table}>
-                                {g.winnerPointsGamer2}
-                            </td>
-                        </tr>
-                    );
-                })
+            return (
+                <tr key={g.id}>
+                    <td>{g.id}</td>
+                    <td>{g.winnerPointsGamer1}</td>
+                    <td>{g.winnerPointsGamer2}</td>
+                </tr>
+            );
+        })
         }
-              <tr>
-                  <td className={styles.table}>
-                      Total
-                  </td>
-                  <td className={styles.table}>
-                      {allPointsGamer1}
-                  </td>
-                  <td className={styles.table}>
-                      {allPointsGamer2}
-                  </td>
-              </tr>
+        <tr>
+            <td>Total</td>
+            <td>
+                {allPointsGamer1}
+            </td>
+            <td>
+                {allPointsGamer2}
+            </td>
+        </tr>
         </thead>
-    </div>
+    </table>
 }
 
 export default ResultTable;
