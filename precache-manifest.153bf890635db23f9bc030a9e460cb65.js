ec33580660b5fdd8c7e7a2609c789bde3da3cb4c
OllenkA <shamko.olga2018@gmail.com>
2020-03-05 09:52:06 +0300

    Updates

---
+++ precache-manifest.153bf890635db23f9bc030a9e460cb65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35b4270b6c57737de274825e5ba7081",
+    "url": "/tick-tack-toe-redux/index.html"
+  },
+  {
+    "revision": "6f579b1377654c59053e",
+    "url": "/tick-tack-toe-redux/static/css/main.5516dec6.chunk.css"
+  },
+  {
+    "revision": "54c21bc3bfd5090811f1",
+    "url": "/tick-tack-toe-redux/static/js/2.98971b70.chunk.js"
+  },
+  {
+    "revision": "6d6c0d1371e570614ef23e12cbfed171",
+    "url": "/tick-tack-toe-redux/static/js/2.98971b70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f579b1377654c59053e",
+    "url": "/tick-tack-toe-redux/static/js/main.7c075489.chunk.js"
+  },
+  {
+    "revision": "150d13cd5acdb9da81ba",
+    "url": "/tick-tack-toe-redux/static/js/runtime-main.5b8e1773.js"
+  }
+]);
